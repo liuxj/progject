0f9305eb44809f2efd7f6da09f55fed4dfffc588
xiangjun liu <liuxiangj@gmail.com>
2009-09-04 18:17:33 +0800

    file operation

--- foo.c
+++ foo.c
@@ -1,9 +1,18 @@
 /*
  * hello there
  * */
-#include <stdio>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+    FILE *sp;
+    char ch;
+    sp = fopen("fileout.txt", "w");
+    do
+    {
+        ch = getchar();
+        fputc(ch, sp);
+    } while (ch != '\n');
+    fclose(sp);
     return printf("hello there\n");
 }
